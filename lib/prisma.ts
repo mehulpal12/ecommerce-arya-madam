49ad532da5110a8a2c2e5504f66166c69e0664da
Anush Kumar Mall <mallanushkumar@gmail.com>
2026-01-27 16:53:48 +0530

    testing2

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,6 @@
+// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-// Prisma Client ko global me store karna
-// Taaki hot reload me naya client na banaye aur memory leak na ho
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
@@ -9,10 +8,9 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["query"], // optional: console me queries dekhne ke liye
+    log: ["query", "error"],
   });
 
-// Development me global attach kar do
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
