9210b91b6f2977f06edb685b3099a8924414d798
Mehul Pal <89767204+mehulpal12@users.noreply.github.com>
2026-02-17 23:47:06 +0530

    d4

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+   experimental: {
+    serverActions: {
+      runtime: "nodejs",
+    },
+  },
   images: {
     remotePatterns: [
       {
